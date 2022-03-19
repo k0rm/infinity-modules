2ab811c34bb8094b9a9ab2749684185097b701a6
k0rm <kormakur20@ru.is>
2022-03-19 20:40:20 +0000

    asdgasgd

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Row } from './Row/Row';
+export { default as Carousel} from './Carousel/Carousel';
+export { default as CartoonNetworkSpinner } from './CartoonNetworkSpinner/CartoonNetworkSpinner';
+export { default as Col } from './Col/Col';
+export { default as Modal } from './Modal/Modal';
